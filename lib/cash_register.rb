79b1f341e87296c4b7a7211c9c90e8415bbe905c
Flatiron School <flatironschool@Flatirons-MacBook-Air.local>
2018-10-11 09:28:37 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :discount, :total
+  attr_reader
+
+  ITEMS = []
+
+  def initialize(employee_discount=0)
+    @total = 0
+    @discount = employee_discount
+    @items = []
+  end
+
+  def add_item(title,price,quantity=1)
+    new_items = price * quantity
+    quantity.times do
+    @items.push(title)
+  end
+    @total+=new_items
+  end
+
+  def apply_discount
+    if @discount != 0
+      float_d = @discount.to_f/100.0
+      actual_discount = @total * float_d
+      @total-=actual_discount
+      @total.to_i
+      return "After the discount, the total comes to $#{total.to_i}."
+    else
+      return "There is no discount to apply."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @total = 0
+  end
+
+end
