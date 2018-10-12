de49091ed48c18c634bd5c28c46a92af15e5ede8
CarsonRey <c.rey494@gmail.com>
2018-10-11 21:33:01 -0400

    made cash register with capability of discounts and voiding transactions

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+require "pry"
+class CashRegister
+  attr_accessor :total, :discount, :quantity
 
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @cart = []
+
+  end
+
+  def add_item(title, price, quantity = 1)
+    quantity.times {@cart.push(title)}
+    @total += price * quantity
+  end
+
+  def apply_discount
+    if @discount == 0
+      "There is no discount to apply."
+    else
+      @total -= (@total *(@discount/100.0)).to_i
+      "After the discount, the total comes to $#{@total}."
+    end
+  end
+
+  def items
+    @cart
+  end
+
+  def void_last_transaction
+    binding.pry
+    @total -= self.total
+  end
+
+end
