365c60b21709df4bb2ddd2c34d1a1a328fc7cb19
achadha2 <andy.chadha2@gmail.com>
2018-10-14 13:34:16 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,6 +4,9 @@ class CashRegister
 attr_accessor :total, :items, :transaction
 attr_reader :discount
 
+# this makes sure each instance initializes with a total, a discount if applicable
+# and an array of items and transactions we can manipulate
+# and use to keep track of inidividual items in the transaction.
 def initialize(disc=0)
   @total = 0
   @discount = disc
@@ -11,6 +14,11 @@ def initialize(disc=0)
   @transaction = []
 end
 
+# Stores previous quantity
+# Adds item into intance of new_register
+# Adds item price in transaction arry for storage
+# calculates the total price and quantity of the items added
+# and updates the total with them
 def add_item(title, price, qty=1)
   old_total = self.total
   qty.times {self.items << title}
@@ -19,6 +27,9 @@ def add_item(title, price, qty=1)
 
 end
 
+# Checks to see if discount is not equal to 0
+# then applies discount to current old_total
+# If no discount, returns error message
 def apply_discount(total="")
   if self.discount !=0
     self.total = self.total - (self.total/100 * self.discount)
@@ -28,8 +39,9 @@ def apply_discount(total="")
   end
 end
 
+# Reverses the last transaction
 def void_last_transaction
-  self.total = self.total - self.transaction[-1]
+  self.total = self.total - (self.transaction[-1]/(1 - self.discount)
   self.items.pop
   self.transaction.pop
   #binding.pry
