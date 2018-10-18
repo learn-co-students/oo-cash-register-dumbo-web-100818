0bf662ee9db67c5121ec60db680bb2878683854c
github username <chao.alexander.123@gmail.com>
2018-10-18 13:01:54 -0400

    defined methods to add item, calculate price and remove last transaction

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
+class CashRegister
+  attr_accessor :total, :discount, :last_transaction, :items
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+    # require 'pry';binding.pry
+    quantity.times {items.push(title)}
+    self.last_transaction = price * quantity
+  end
+
+  def discount_inverse
+    (100 - discount) * 0.01
+  end
+
+  def apply_discount
+    return "There is no discount to apply." if self.discount == 0
+    self.total = self.total * discount_inverse
+    return "After the discount, the total comes to $#{self.total.round()}."
+  end
+
+  def void_last_transaction
+    self.total = self.total - last_transaction
+  end
+end
